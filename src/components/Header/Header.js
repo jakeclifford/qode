fea89d126a32bab3c2f1c6bc0646a49a4474111d
Jake Clifford <jakeclifford35@gmail.com>
2022-06-04 18:42:19 +1000

    center design selements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,9 @@ import "./Header.scss"
 export default function Header(){
     return (
         <div id="navbar">
-            <img src={Logo} id="logo"></img>
+            <a href="/">
+                <img src={Logo} id="logo"></img>
+            </a>
         </div>
     )
 
